49930d1d12ff2aaa5d8152f9c9ec9a3be14ed0b8
Dev-Capivara <pauloalmeida1459.staff@gmail.com>
2026-03-11 16:41:28 -0400

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,8 +1,5 @@
-import sql from './db.js';
+import { neon } from '@neondatabase/serverless';
 
-export default async function handler(req, res) {
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+const sql = neon(process.env.DATABASE_URL);
 
-}
+export default sql;
