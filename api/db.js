077c291f80f18974391f5f2c61f635ea608a9185
Dev-Capivara <pauloalmeida1459.staff@gmail.com>
2026-03-11 16:39:40 -0400

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,1 +1,8 @@
+import sql from './db.js';
 
+export default async function handler(req, res) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+}
